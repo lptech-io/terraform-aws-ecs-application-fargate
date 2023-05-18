7b7b284697e16fab12c9b1beb2492d89a1ccf918
Luca Pisciotta <luca.pisciotta@lptech.io>
2023-05-18 13:08:12 +0200

    Add slow start for target group

--- modules/target-group/main.tf
+++ modules/target-group/main.tf
@@ -14,6 +14,7 @@ resource "aws_lb_target_group" "target_group" {
   name                          = var.target_group_name
   port                          = 80
   protocol                      = "HTTP"
+  slow_start                    = 30
   target_type                   = "ip"
   vpc_id                        = var.vpc_id
 
