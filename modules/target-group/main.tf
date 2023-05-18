4c0513a510dd78024dbba1709fe6a272b7aa1df6
Luca Pisciotta <luca.pisciotta@lptech.io>
2023-05-18 17:19:09 +0200

    Remove name from target group

--- modules/target-group/main.tf
+++ modules/target-group/main.tf
@@ -11,7 +11,6 @@ terraform {
 resource "aws_lb_target_group" "target_group" {
   deregistration_delay          = var.deregistration_delay
   load_balancing_algorithm_type = "least_outstanding_requests"
-  name                          = var.target_group_name
   port                          = 80
   protocol                      = "HTTP"
   protocol_version              = "HTTP2"
@@ -28,6 +27,9 @@ resource "aws_lb_target_group" "target_group" {
     protocol            = var.health_check.protocol
     timeout             = var.health_check.timeout
     unhealthy_threshold = var.health_check.unhealthy_threshold
+  }
+  tags = {
+    "Name" = var.target_group_name
   }
   lifecycle {
     create_before_destroy = true
