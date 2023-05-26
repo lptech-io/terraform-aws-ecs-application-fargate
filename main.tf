c00bcf134005e7fe658e5c01c87bc1e11506fa4a
Luca Pisciotta <luca.pisciotta@lptech.io>
2023-05-26 10:53:07 +0200

    Fix listener rule configuration to support null value on custom headers

--- main.tf
+++ main.tf
@@ -90,7 +90,7 @@ resource "aws_lb_listener_rule" "public_url_on_443" {
     }
   }
   dynamic "condition" {
-    for_each = var.listener_rule_configuration.headers
+    for_each = var.listener_rule_configuration.headers != null ? [var.listener_rule_configuration.headers] : []
     content {
       http_header {
         http_header_name = condition.value.name
